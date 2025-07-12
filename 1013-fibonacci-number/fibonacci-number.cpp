0749e5d2e2714d855ca8f0714ee5bbfa214bb217
SaiyyedKaif <146384933+SaiyyedKaif@users.noreply.github.com>
2025-07-13 00:00:24 +0530

    Time: 2 ms (77.60%) | Memory: 7.8 MB (63.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -8,23 +8,17 @@
 
 	        if (n == 1) return 1;
 
-	 
+	        int prev = 0;
 
-	        std::vector<int> dp(n + 1);
-
-	        dp[0] = 0;
-
-	        dp[1] = 1;
-
-	 
+	        int curr = 1;
 
 	        for (int i = 2; i <= n; ++i) {
-
-	            dp[i] = dp[i - 1] + dp[i - 2];
-
+                int next =  prev + curr ;
+                prev = curr ;
+                curr = next ;
 	        }
 
-	        return dp[n];
+	        return curr;
 
 	    }
 
