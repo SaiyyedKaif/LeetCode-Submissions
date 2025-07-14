97752fb7ddb46b81a49d5a8766bd606fe72bf962
SaiyyedKaif <146384933+SaiyyedKaif@users.noreply.github.com>
2025-07-15 02:06:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (80.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,4 +1,7 @@
-class Solution {
+
+// YE WALA CODE O(n) TIME COMPLEXITY LETA HAI AND O(n) SPACE COMPLEXITY
+
+/* class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -16,5 +19,30 @@ public:
             dp[i] = max(dp[i-1] , dp[i-2]+nums[i]);
         }
         return dp[n-1];
+    }
+}; */
+
+// ABHI HAM ISKO OPTIMIZE KARENEGE BY REDUCING THE SPACE COMPLEXITY TO O(n) i.e TIME -> O(n) SPACE -> O(n)
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        else if (n == 2){
+            return max(nums[0] , nums[1]);
+        }
+        
+        int prev = nums[0];
+        int curr = max(nums[0] , nums[1]);
+
+        for(int i = 2 ; i < n ; i++){
+            int next = max(curr ,prev +nums[i]);
+            prev = curr ;
+            curr = next ;
+        }
+        return curr;
     }
 };
